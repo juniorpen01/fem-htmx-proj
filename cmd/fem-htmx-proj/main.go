b4e5edd30bea717398a490f926bf65127e3a9436
juniorpen01 <01juniorpen@gmail.com>
2025-12-17 23:57:58 +0800

    Set up htmx and add button that increments instead

--- cmd/fem-htmx-proj/main.go
+++ cmd/fem-htmx-proj/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"text/template"
@@ -15,15 +16,18 @@ const PORT = "localhost:42069"
 func main() {
 	count := Count{}
 
-	http.HandleFunc("/{$}", func(w http.ResponseWriter, r *http.Request) {
-		tmpl, err := template.ParseFiles("web/template/index.html")
-		if err != nil {
-			log.Fatalln(err)
-		}
+	tmpl, err := template.ParseFiles("web/template/index.html")
+	if err != nil {
+		log.Fatalln(err)
+	}
 
+	http.HandleFunc("GET /{$}", func(w http.ResponseWriter, r *http.Request) {
 		tmpl.Execute(w, count)
+	})
 
+	http.HandleFunc("POST /count/{$}", func(w http.ResponseWriter, r *http.Request) {
 		count.Count++
+		fmt.Fprintf(w, "Count %d", count.Count)
 	})
 
 	// start server
