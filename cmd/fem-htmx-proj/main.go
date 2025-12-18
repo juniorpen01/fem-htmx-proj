d4f4e289af75eb3de79b9e1999526aa8c7f999c5
juniorpen01 <01juniorpen@gmail.com>
2025-12-18 18:29:35 +0800

    I don't understand what's going on

--- cmd/fem-htmx-proj/main.go
+++ cmd/fem-htmx-proj/main.go
@@ -12,21 +12,26 @@ import (
 type (
 	Count    int
 	Contacts []contact_store.Contact
+	Form     struct {
+		Values contact_store.Contact
+		Error  string
+	}
 )
 
 func main() {
 	const PORT = "localhost:42069"
 
-	mockContactStore := contact_store.Contacts{}
+	contactStore := contact_store.Contacts{}
 
-	mockContactStore.Add(contact_store.Contact{Name: "idiot", Email: "idiot@gmail.com"})
-	mockContactStore.Add(contact_store.Contact{Name: "dummkopf", Email: "dummkopf@gmail.com"})
-	mockContactStore.Add(contact_store.Contact{Name: "therealdummkopf", Email: "dummkopf@gmail.com"})
+	contactStore.Add(contact_store.Contact{Name: "idiot", Email: "idiot@gmail.com"})
+	contactStore.Add(contact_store.Contact{Name: "dummkopf", Email: "dummkopf@gmail.com"})
+	contactStore.Add(contact_store.Contact{Name: "therealdummkopf", Email: "dummkopf@gmail.com"})
 
 	data := struct {
 		Count
 		Contacts
-	}{}
+		Form
+	}{Contacts: contactStore.Contacts()}
 
 	tmpl, err := template.ParseFiles("web/template/index.html")
 	if err != nil {
@@ -47,14 +52,19 @@ func main() {
 	router.HandleFunc("POST /contacts/{$}", func(w http.ResponseWriter, r *http.Request) {
 		contact := contact_store.Contact{Name: r.FormValue("name"), Email: r.FormValue("email")}
 
-		if err := mockContactStore.Add(contact); err != nil {
-			http.Error(w, err.Error(), http.StatusConflict)
+		if err := contactStore.Add(contact); err != nil {
+			data.Values.Name = contact.Name
+			data.Values.Email = contact.Email
+			data.Error = err.Error()
+			w.WriteHeader(http.StatusConflict)
+			tmpl.ExecuteTemplate(w, "contact-form", data)
 			return
+		} else {
+			data.Contacts = contactStore.Contacts()
+
+			tmpl.ExecuteTemplate(w, "contact-form", data)
+			tmpl.ExecuteTemplate(w, "oob-contact", contact)
 		}
-
-		data.Contacts = mockContactStore.Contacts()
-
-		tmpl.ExecuteTemplate(w, "contacts", data)
 	})
 
 	// start server
