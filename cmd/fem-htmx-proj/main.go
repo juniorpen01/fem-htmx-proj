a15f17c11026ca2c320fe8db53c6296ae57d6038
juniorpen01 <01juniorpen@gmail.com>
2025-12-20 14:57:41 +0800

    Implement server that starts an app that increments a count on refresh

--- cmd/fem-htmx-proj/main.go
+++ cmd/fem-htmx-proj/main.go
@@ -1,4 +1,9 @@
 package main
 
+import (
+	"github.com/juniorpen01/fem-htmx-proj/internal/server"
+)
+
 func main() {
+	server.Run()
 }
---
+++ internal/server/server.go
@@ -0,0 +1,35 @@
+// Package server
+package server
+
+import (
+	"log"
+	"net/http"
+	"text/template"
+)
+
+const addr = ":8080"
+
+func Run() {
+	// template
+	tmpl := template.Must(template.ParseFiles("web/template/index.html"))
+
+	// state
+	count := struct{ Count int }{}
+
+	// routes
+	http.HandleFunc("GET /{$}", func(w http.ResponseWriter, r *http.Request) {
+		tmpl.Execute(w, count)
+		count.Count++
+	})
+
+	// start
+	log.Printf("server started on %s", addr)
+	log.Fatal(http.ListenAndServe(addr, logger(http.DefaultServeMux)))
+}
+
+func logger(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s", r.Method, r.URL)
+		next.ServeHTTP(w, r)
+	})
+}
