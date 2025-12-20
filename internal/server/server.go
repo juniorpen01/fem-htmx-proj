4f2d5ecd4894e1fd836a2f8e1bed856185a2dfad
juniorpen01 <01juniorpen@gmail.com>
2025-12-20 17:52:19 +0800

    Implement form that appends contacts to a list

--- internal/server/server.go
+++ internal/server/server.go
@@ -8,6 +8,11 @@ import (
 	"text/template"
 )
 
+type contact struct {
+	Name, Email string
+}
+type contacts []contact
+
 const addr = ":8080"
 
 func Run() {
@@ -15,7 +20,10 @@ func Run() {
 	tmpl := template.Must(template.ParseFiles("web/template/index.html"))
 
 	// state
-	count := struct{ Count int }{}
+	data := struct {
+		Count    int
+		Contacts contacts
+	}{Contacts: contacts{{"idiot@gmail.com", "idiot@gmail.com"}, {"dummkopf", "dummkopf101@gmail.com"}}}
 
 	// routes
 
@@ -23,12 +31,22 @@ func Run() {
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("web/static"))))
 
 	http.HandleFunc("GET /{$}", func(w http.ResponseWriter, r *http.Request) {
-		tmpl.Execute(w, count)
+		tmpl.Execute(w, data)
 	})
 
 	http.HandleFunc("POST /count", func(w http.ResponseWriter, r *http.Request) {
-		count.Count++
-		fmt.Fprint(w, count.Count)
+		data.Count++
+		fmt.Fprint(w, data.Count)
+	})
+
+	http.HandleFunc("POST /contacts", func(w http.ResponseWriter, r *http.Request) {
+		name := r.FormValue("name")
+		email := r.FormValue("email")
+
+		newContact := contact{name, email}
+		data.Contacts = append(data.Contacts, newContact)
+
+		tmpl.ExecuteTemplate(w, "contact", newContact)
 	})
 
 	// start
